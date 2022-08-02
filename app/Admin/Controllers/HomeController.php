b64a02c35c344e955c2610976ee7210dfe86aa4e
zhusq <18036161805@163.com>
2022-08-02 10:34:54 +0800

    查看登录用户信息的按钮

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Admin\Controllers;
 
+use App\Admin\Actions\ShowUser;
 use App\Admin\Metrics\Examples;
 use App\Http\Controllers\Controller;
 use Dcat\Admin\Http\Controllers\Dashboard;
@@ -11,26 +12,35 @@ use Dcat\Admin\Layout\Row;
 
 class HomeController extends Controller
 {
+
     public function index(Content $content)
     {
-        return $content
-            ->header('Dashboard')
-            ->description('Description...')
-            ->body(function (Row $row) {
-                $row->column(6, function (Column $column) {
-                    $column->row(Dashboard::title());
-                    $column->row(new Examples\Tickets());
-                });
-
-                $row->column(6, function (Column $column) {
-                    $column->row(function (Row $row) {
-                        $row->column(6, new Examples\NewUsers());
-                        $row->column(6, new Examples\NewDevices());
-                    });
-
-                    $column->row(new Examples\Sessions());
-                    $column->row(new Examples\ProductOrders());
-                });
-            });
+        return $content->body(ShowUser::make());
     }
+
+
+
+
+//    public function index(Content $content)
+//    {
+//        return $content
+//            ->header('Dashboard')
+//            ->description('Description...')
+//            ->body(function (Row $row) {
+//                $row->column(6, function (Column $column) {
+//                    $column->row(Dashboard::title());
+//                    $column->row(new Examples\Tickets());
+//                });
+//
+//                $row->column(6, function (Column $column) {
+//                    $column->row(function (Row $row) {
+//                        $row->column(6, new Examples\NewUsers());
+//                        $row->column(6, new Examples\NewDevices());
+//                    });
+//
+//                    $column->row(new Examples\Sessions());
+//                    $column->row(new Examples\ProductOrders());
+//                });
+//            });
+//    }
 }
