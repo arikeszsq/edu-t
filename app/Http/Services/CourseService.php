18ee8ae0576c3e629e77134f5d6a2d538e949bec
zhusq <18036161805@163.com>
2022-09-05 14:18:32 +0800

    course list

--- app/Http/Services/CourseService.php
+++ app/Http/Services/CourseService.php
@@ -15,15 +15,15 @@ use Illuminate\Support\Facades\DB;
 class CourseService
 {
     use ImageTrait;
+
     public function lists($inputs)
     {
-        $list = [];
         $activity_id = $inputs['activity_id'];
 
         $query = DB::table('activity_sign_com as com')
             ->leftJoin('company_course as course', 'com.company_id', 'course.company_id')
-            ->leftJoin('company','course.company_id','company.id')
-            ->select(['course.id as id','course.logo','course.name','course.price','course.total_num','course.sale_num',
+            ->leftJoin('company', 'course.company_id', 'company.id')
+            ->select(['course.id as id', 'course.logo', 'course.name', 'course.price', 'course.total_num', 'course.sale_num',
                 'company.name as company_name'])
             ->where('com.activity_id', $activity_id);
         if (isset($inputs['type']) && $inputs['type']) {
@@ -31,16 +31,17 @@ class CourseService
         }
         $lists = $query->get();
 
-        $course =[];
+        $course = [];
         foreach ($lists as $list) {
             $course[] = [
-                'id'=>$list->id,
+                'id' => $list->id,
                 'logo' => $this->fullImgUrl($list->logo),
                 'name' => $list->name,
-                'price' =>$list->price,
+                'price' => $list->price,
                 'total_num' => $list->total_num,
                 'sale_num' => $list->sale_num,
-                'company_name' =>$list->company_name,
+                'company_name' => $list->company_name,
+                'gap' => '0km'
             ];
         }
 
