f5b3ff5bf0203cb05721adf0aab3e05b1e3764fd
zhusq <18036161805@163.com>
2022-09-09 17:19:25 +0800

    小程序------------------地图设置家的位置

--- mini/crazy/app.js
+++ mini/crazy/app.js
@@ -32,16 +32,14 @@ App({
         wx.getSetting({
           success: res => {
             if (res.authSetting['scope.userInfo']) {
-              console.log(1)
               // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
               wx.getUserInfo({
                 success: res => {
-                  console.log(2)
                   // 可以将 res 发送给后台解码出 unionId
                   this.globalData.userInfo = res.userInfo
                   this.globalData.hasUserInfo = true
                   if (!this.checkIsLogin()) {
-                    console.log(3)
+                    console.log('no login');
                     this.getToken(code, res.encryptedData, res.iv);
                   }
 
