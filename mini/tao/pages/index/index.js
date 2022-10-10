4551ed9a24911a2fdc1f7a6088cd0564fc9aa726
zhusq <18036161805@163.com>
2022-10-10 17:17:22 +0800

    根据二维码跳详情页

--- mini/tao/pages/index/index.js
+++ mini/tao/pages/index/index.js
@@ -1,42 +1,77 @@
 // 获取应用实例
-const app = getApp()
+const app = getApp();
 
 Page({
-  data: {
-    news: {}
-  },
-  onLoad() {
-    this.getList();
-  },
-  
-  toDetail:function(event){
-    var id = event.currentTarget.dataset.id;
-    var one= event.currentTarget.dataset.one;
-    if(one==1){
-        wx.redirectTo({
-            url:'../activity/one/index?id='+id
-          });
-    }else{
-        wx.redirectTo({
-            url:'../activity/many/index?id='+id
-          });
+    data: {
+        news: {}
+    },
+    onLoad(query) {
+
+        // scene 需要使用 decodeURIComponent 才能获取到生成二维码时传入的 scene
+        const scene = decodeURIComponent(query.scene)
+        if (scene !== 'undefined') {
+            console.log(scene);
+            var res = scene.split(',');
+            var res_length = res.length;
+            var activity_id = res[0];
+            app.globalData.activity_id = activity_id;
+            if (res_length >= 2) {
+                var share_user_id = res[1];
+                app.globalData.share_user_id = share_user_id
+            }
+            this.toActivityDetail(activity_id);
+        }
+        console.log('no scene');
+        this.getList();
+    },
+
+    toDetail: function (event) {
+        var id = event.currentTarget.dataset.id;
+        var one = event.currentTarget.dataset.one;
+        if (one === 1) {
+            wx.redirectTo({
+                url: '../activity/one/index?id=' + id
+            });
+        } else {
+            wx.redirectTo({
+                url: '../activity/many/index?id=' + id
+            });
+        }
+    },
+
+    toActivityDetail:function(id){
+        app.apiRequest({
+            url: '/activity/type/'+id,
+            method: 'get',
+            data: {},
+            success: res => {
+                var type = res.data.response;
+                if (type === 1) {
+                    wx.redirectTo({
+                        url: '../activity/one/index?id=' + id
+                    });
+                } else {
+                    wx.redirectTo({
+                        url: '../activity/many/index?id=' + id
+                    });
+                }
+            }
+        });
+    },
+
+    getList: function () {
+        app.apiRequest({
+            url: '/activity/lists',
+            method: 'get',
+            data: {},
+            success: res => {
+                var that = this;
+                console.log(res.data);
+                that.setData({
+                    news: res.data.response
+                })
+            }
+        });
     }
-  },
 
-  getList: function () {
-    app.apiRequest({
-      url: '/activity/lists',
-      method: 'get',
-      data: {
-      },
-      success: res => {
-        var that = this;
-        console.log(res.data)
-        that.setData({
-          news: res.data.response
-        })
-      }
-    });
-  }
-
-})
+});
